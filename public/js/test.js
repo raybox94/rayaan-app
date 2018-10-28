d8cd73acc3a6e9666cac7f1e42a3a0d5ed1d1ab7
Rayaan Ahmed <raybox94@gmail.com>
2018-10-28 13:56:58 -0500

    more test cases included

--- public/js/test.js
+++ public/js/test.js
@@ -17,6 +17,20 @@ QUnit.test('Testing AI function with negitive number sets of inputs', function (
     assert.equal(ai(555),0,'Tested with large -ve number -555');
 });
 
+QUnit.test('Testing AI function with floating point number sets of inputs', function (assert) {
+
+    assert.equal(ai(5.67),0,'Tested with small floating point number 5.67');
+    assert.equal(ai(1.0),0,'Tested with small floating point number 1.0');
+    assert.equal(ai(555.55555),0,'Tested with large floting number 555.555');
+});
+
+QUnit.test('Testing AI function with negitive floating point number sets of inputs', function (assert) {
+
+    assert.equal(ai(-5.67),0,'Tested with small -ve floating point number -5.67');
+    assert.equal(ai(-1.0),0,'Tested with small -ve floating point number -1.0');
+    assert.equal(ai(-555.55555),0,'Tested with -ve large floting number -555.555');
+});
+
 QUnit.test('Testing AI function with String', function (assert) {
 
     assert.throws(function () { ai('a'); }, /The given argument is not a number/, 'Passing in a string correctly raises an Error');
